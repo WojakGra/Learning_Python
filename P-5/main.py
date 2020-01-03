5609fb3616e7be8caeb5457f87845e77832e6241
WojakGra <kubaxplay@o2.pl>
2020-01-03 21:48:54 +0100

    Zadania 5 na konkurs KOALA
    P-5

---
+++ P-5/main.py
@@ -0,0 +1,65 @@
+# Słownik
+Characters = {
+    'A':0,
+    'Ą':1,
+    'B':2,
+    'C':3,
+    'Ć':4,
+    'D':5,
+    'E':6,
+    'Ę':7,
+    'F':8,
+    'G':9,
+    'H':10,
+    'I':11,
+    'J':12,
+    'K':13,
+    'L':14,
+    'Ł':15,
+    'M':16,
+    'N':17,
+    'Ń':18,
+    'O':19,
+    'Ó':20,
+    'P':21,
+    'Q':22,
+    'R':23,
+    'S':24,
+    'Ś':25,
+    'T':26,
+    'U':27,
+    'W':28,
+    'V':29,
+    'X':30,
+    'Y':31,
+    'Z':32,
+    'Ź':33,
+    'Ż':34
+}
+
+# Funkcja do pobierania value z key w słowniku
+def get_key(val):
+    for key, value in Characters.items():
+        if val == value:
+            return key
+
+# Wczytanie danych
+inputList = list(input("Podaj ciąg: "))
+inputNumber = int(input("Podaj przesuniecie: "))
+
+# Przesuwanie znaków
+outputList = []
+for x in range(len(inputList)):
+    characterNumber = Characters.get(inputList[x]) + inputNumber
+    if(characterNumber > 34):
+        characterNumber -= 35
+        outputList.append(get_key(characterNumber))
+    elif(characterNumber < 0):
+        characterNumber += 35
+        outputList.append(get_key(characterNumber))
+    else:
+        outputList.append(get_key(characterNumber))
+
+# Wypisanie wyniku
+for i in outputList:
+    print(i, end="")
