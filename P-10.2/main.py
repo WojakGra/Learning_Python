82d9b7fec87b48dcf5556aace173c62bfc2f1825
WojakGra <kubaxplay@o2.pl>
2020-01-05 16:32:52 +0100

    Zadanie 10 na konkurs KOALA (do wersji nieprogramistycznej)

---
+++ P-10.2/main.py
@@ -0,0 +1,41 @@
+# Funkcja do przetworzenia wartości tak jak zostało to podane w zadaniu
+def ChangeContrast(numbers, change):
+    newnumbers = []
+    global lighter, darker
+    lighter, darker = 0,0
+    for constrast in numbers:
+        constrast = int((constrast - 128) * change) + 128
+        if constrast > 255:
+            constrast = 255
+        elif constrast < 0:
+            constrast = 0
+        newnumbers.append(constrast)
+    print(newnumbers)
+    for x, y in zip(numbers, newnumbers):
+        if x < y:
+            lighter += 1
+        elif x > y:
+            darker += 1
+        else:
+            continue
+
+# Funkcja do zmiany z binarnych liczb na decymalne
+def ChangeDecToBin(list):
+    newlist = []
+    for number in list:
+        number = int(number, 2)
+        newlist.append(number)
+    print(newlist)
+    return newlist
+
+
+
+# Wprowadzanie danych
+inputUser = input("Podaj ciąg: ")
+changeInt = float(input("Podaj liczbę o ile chcesz zmienić kontrast: "))
+inputUser = inputUser.split()
+
+# Wyprowadzanie danych
+ChangeContrast(ChangeDecToBin(inputUser), changeInt)
+print("Ilość jaśnieszych pikseli to "+str(lighter))
+print("Ilość ciemniejszych pikseli to "+str(darker))
