d43ac27ee4be34f2bd06870e499ddebc5cd2cc69
WojakGra <kubaxplay@o2.pl>
2020-01-20 23:03:41 +0100

    Trying to do something

--- P-15/main.py
+++ P-15/main.py
@@ -1,22 +1,29 @@
 import math  # Importowanie fukncji matematycznych
 
 
-def ChangePositionNumber(n, k):
+def ChangePositionNumber(n, k):  # n - Cyclists, k - Changes
     return int(math.factorial(n) / (math.factorial(k) * math.factorial(n - k)))
+
+
+def HowManyChangesPossible(n, k, c):  # n - Cyclists, k - Changes, c - ChangePositionNumber
+    for ChangePositionNumber in range(k):
+        for Cyclists in range(n):
+            # Try to implement adding a table with all cyclists
+
+    for ChangePositionNumber in range(c):
+        for Cyclists in range(n):
+            # Try to implement a finding out that cyclist is where
 
 
 Cyclists = int(input("Podaj ilość kolarzy: "))
 Changes = int(input("Podaj ilość zmiany prowadzącego: "))
 
 if Changes == 0:
-    print("Ilość możliwości to " + str(math.factorial(Cyclists-1)))
+    print("Ilość możliwości to " + str(math.factorial(Cyclists - 1)))
     input("\n\nWciśnij Enter, aby kontynuować...")
     exit()
 
-number = ChangePositionNumber(Cyclists - 1, Changes)
+print(HowManyChangesPossible(Cyclists, Changes, ChangePositionNumber(Cyclists - 1, Changes)))
 
-print(number)
-
-# Wstrzymanie programu
-input("\n\nWciśnij Enter, aby kontynuować...")
+input("\n\nWciśnij Enter, aby kontynuować...")  # Wstrzymanie programu
 exit()
