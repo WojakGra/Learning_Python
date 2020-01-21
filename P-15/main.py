31de614700881ce97b4ec1fdcff5c640f0ca8c03
WojakGra <kubaxplay@o2.pl>
2020-01-21 23:34:33 +0100

    Ehh

--- P-15/main.py
+++ P-15/main.py
@@ -5,25 +5,40 @@ def ChangePositionNumber(n, k):  # n - Cyclists, k - Changes
     return int(math.factorial(n) / (math.factorial(k) * math.factorial(n - k)))
 
 
-def HowManyChangesPossible(n, k, c):  # n - Cyclists, k - Changes, c - ChangePositionNumber
-    for ChangePositionNumber in range(k):
-        for Cyclists in range(n):
-            # Try to implement adding a table with all cyclists
+def HowManyChanges(n, k, c):  # n - Cyclists, k - Changes, c - ChangePositionNumber
+    CyclistsList = []
 
+    TempList = [1]
+    for Changes in range(k):
+        for Cyclists in range(2, n + 1):
+            if Cyclists > TempList[0]:
+                TempList.insert(0, Cyclists)
+                break
+    CyclistsList.append(TempList)
+
+    for ChangePositionNumber in range(1, c):
+        TempList = CyclistsList[ChangePositionNumber-1]
+        TempList[][] = Cyclist
+        CyclistsList.append(TempList)
+
+    return CyclistsList
+
+
+"""
     for ChangePositionNumber in range(c):
         for Cyclists in range(n):
             # Try to implement a finding out that cyclist is where
-
+"""
 
 Cyclists = int(input("Podaj ilość kolarzy: "))
 Changes = int(input("Podaj ilość zmiany prowadzącego: "))
 
-if Changes == 0:
+if Changes == 0:  # Wypisywanie wyniku jeżeli zmiana prowadzącego to 0
     print("Ilość możliwości to " + str(math.factorial(Cyclists - 1)))
     input("\n\nWciśnij Enter, aby kontynuować...")
     exit()
 
-print(HowManyChangesPossible(Cyclists, Changes, ChangePositionNumber(Cyclists - 1, Changes)))
+print(HowManyChanges(Cyclists, Changes, ChangePositionNumber(Cyclists - 1, Changes)))
 
 input("\n\nWciśnij Enter, aby kontynuować...")  # Wstrzymanie programu
 exit()
